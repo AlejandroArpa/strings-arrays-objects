0a0a9269e2a14ea38740d262584b3e6e1efee6df
Alejandro Ramirez <alejorp195@hotmail-com>
2024-04-19 00:36:32 -0500

    Section1 first challenge complete

--- taller-3/Seccion1/Reto1.js
+++ taller-3/Seccion1/Reto1.js
@@ -1,5 +1,6 @@
 let fullName = prompt("Ingrese su nombre completo");
-const users = [];
+const users = {};
+const domain = "@myDomain.com";
 
 const createUser = (name) =>{
     let fullName = name.toLowerCase();
@@ -7,21 +8,44 @@ const createUser = (name) =>{
     let nameThreeFirsLetters = arrayFullName[0].slice(0, 3); 
     let lastNameThreeFirsLetters = arrayFullName[1].slice(0, 3); 
     let userName = nameThreeFirsLetters.concat(lastNameThreeFirsLetters);
-    let userEmail = userName.concat("@myDomain.com");
+    let userEmail = userName.concat(domain);
     return{
-        [userName]:userEmail
+        [userName] : userEmail
     }
 }
 
-const validateUser = (name) =>{
-    const userObj = createUser(fullName);
-    console.log("Holi")
-    console.log(userObj.key[0]);
-    // users.forEach(user => {
-    //     if (user?.(userObj.key[0])){
-
-    //     }
-    // })
+const appendUser = (user) =>{
+    Object.assign(users, user);
 }
 
+const validateUser = (name) =>{
+    let userObj = createUser(name);
+    if (Object.keys(users).length === 0){
+       appendUser(userObj);
+    }
+    else{
+
+        if (Object.keys(users).includes(Object.keys(userObj)[0])){
+            let name = Object.keys(userObj)[0];
+            newName = name.concat('1');
+            console.log(name); 
+            let newEmail = newName.concat(domain);
+            const userParce ={
+                [newName] : newEmail 
+            }
+            appendUser(userParce);
+        }
+        else{
+            appendUser(userObj);
+        }
+    } 
+}
+
+// Number.isNaN String Boolean Object
+
 validateUser(fullName);
+fullName = prompt("Ingrese su nombre completo");
+validateUser(fullName);
+fullName = prompt("Ingrese su nombre completo");
+validateUser(fullName);
+console.log(users);
